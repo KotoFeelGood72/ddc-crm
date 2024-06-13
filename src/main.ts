a9935d31279bc36a2fd60bc547cdecef3a892e20
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-06-13 21:08:02 +0300

    ыы

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
+console.log('Vue app is being created');
 const pinia = createPinia()
 const app = createApp(App)
 
