cf2b565da4a6412ca0803b5968a57e58996960c0
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-06-13 23:31:13 +0300

    ыы

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="Внесли изменения"/>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
-console.log('Creating Vue app'); // Log for debugging
 
 const pinia = createPinia();
 const app = createApp(App);
 
-console.log('Mounting Vue app'); // Log for debugging
 
 app.use(pinia);
 app.mount('#app');
 
-console.log('Vue app mounted'); // Log for debugging
+
