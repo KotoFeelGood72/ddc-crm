137fb63547fc8b62dba14d679da2abbf51860072
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-06-13 23:28:35 +0300

    s

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  // transpileDependencies: true,
+  transpileDependencies: true,
   filenameHashing: false,
-  publicPath: '/wp-content/themes/manager/vue/dist/'
+  publicPath: '/wp-content/themes/manager/vue/dist/',
+  outputDir: path.resolve(__dirname, './dist'),
+  css: {
+    extract: true
+  },
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].js',
+      chunkFilename: 'js/[name].js',
+    },
+  },
+  chainWebpack: config => {
+    config.plugins.delete('html');
+    config.plugins.delete('preload');
+    config.plugins.delete('prefetch');
+  }
 });
